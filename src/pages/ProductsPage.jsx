6f20bc19863f2497ee3aff38c463fff1c5fc756d
Salvo-Raia <salvo.raia@outlook.it>
2026-02-18 19:47:38 +0100

    Add axios + fetch fake products

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,7 +1,20 @@
-import Header from "../assets/components/Header"
+import axios from "axios"
+import { useState, useEffect } from "react"
+const fakeStoreProducts = "https://fakestoreapi.com/products"
 
 export default function ProductsPage () {
+    const [fakeProducts, setFakeProducts] = useState ([""]);
+    const fetchProducts = () => {
+        axios.get(fakeStoreProducts)
+             .then((res) => {
+                console.log(res.data);
+                setFakeProducts(res.data)
+             })
+    }
+
+    useEffect(fetchProducts, []);
+
     return <>
            <p>Varie card prodotti Coming soon</p>
-    </>
+           </>
 }
