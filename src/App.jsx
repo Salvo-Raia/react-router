21cf8164ed3743f67ae239f03edf584a809b38ea
Salvo-Raia <salvo.raia@outlook.it>
2026-02-18 18:39:32 +0100

    Add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, createBrowserRouter } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
 
 export default function App() {
   return <BrowserRouter>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,9 @@
+import Header from "../assets/components/Header" 
+
 export default function HomePage () {
     return (
         <main>
+            <Header />
             <div>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi fugit dolores illum quae ullam aliquam dolorum, recusandae iste repudiandae praesentium delectus odit, amet tempora eaque quidem ipsam veniam, quia minima!</p>
             </div>
