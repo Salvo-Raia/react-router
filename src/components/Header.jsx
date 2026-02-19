7ce92c422a79270d72a02b97e213c823943df348
Salvo-Raia <salvo.raia@outlook.it>
2026-02-19 19:18:01 +0100

    Minor style changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,45 @@
-import { NavLink } from "react-router-dom"
-export default function Header () {
-    return <header>
-        <nav className="navbar navbar-expand-lg bg-success ">
-  <div className="container-fluid">
-    <NavLink className="navbar-brand" to="/">
-      <img src="public\logo.png" alt="FakeStore logo" title="FakeStore logo"/>
-    </NavLink>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-      <div className="navbar-nav text-white">
-        <NavLink to="/" className="nav-link">Home</NavLink>
-        <NavLink to="/products" className="nav-link">Products</NavLink>
-        <NavLink to="/about-us" className="nav-link">About us</NavLink>
-      </div>
-    </div>
-  </div>
-</nav>
+import { NavLink } from "react-router-dom";
+export default function Header() {
+  return (
+    <header>
+      <nav className="navbar navbar-expand-lg bg-success ">
+        <div className="container-fluid">
+          <NavLink className="navbar-brand" to="/">
+            <img
+              src="public\logo.png"
+              alt="FakeStore logo"
+              title="FakeStore logo"
+            />
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse justify-content-end"
+            id="navbarNavAltMarkup"
+          >
+            <div className="navbar-nav text-white">
+              <NavLink to="/" className="nav-link">
+                Home
+              </NavLink>
+              <NavLink to="/products" className="nav-link">
+                Products
+              </NavLink>
+              <NavLink to="/about-us" className="nav-link">
+                About us
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </nav>
     </header>
+  );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,18 +6,20 @@ export default function HomePage() {
       <div className="products-showcase">
         <h2 className="h3 my-4">Give a look to our brand new products!</h2>
 
-        <NavLink to="/products" className="text-decoration-none">
-          <div className="card">
-            <img
-              src="src\assets\img\Shop.jpg"
-              className="card-img-top"
-              alt="FakeStore Shop"
-            />
+        <div className="card">
+          <img
+            src="src\assets\img\Shop.jpg"
+            className="card-img-top"
+            alt="FakeStore Shop"
+          />
+          <NavLink to="/products" className="text-decoration-none">
             <div className="card-body bg-success text-center">
-              <h5 className="card-title">Find out our product gallery</h5>
+              <h5 className="card-title text-white">
+                Find out our product gallery!
+              </h5>
             </div>
-          </div>
-        </NavLink>
+          </NavLink>
+        </div>
       </div>
     </>
   );
