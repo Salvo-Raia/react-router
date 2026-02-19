9c408b99ca87408fc66662153dd4870b2485c153
Salvo-Raia <salvo.raia@outlook.it>
2026-02-19 16:53:45 +0100

    Fetch proper product into product detail page

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -1,6 +1,18 @@
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import axios from "axios";
+const fakeStoreProducts = "https://fakestoreapi.com/products";
 
 export default function ProductDetailPage() {
   const { id } = useParams();
-  return <h2>Ti mostro la scheda del prodotto {id}</h2>;
+
+  const fetchProducts = () => {
+    axios.get(fakeStoreProducts).then((res) => {
+      console.log(res.data[id - 1]);
+    });
+  };
+
+  useEffect(() => fetchProducts);
+
+  return <h2>Ti mostro la scheda del prodotto </h2>;
 }
