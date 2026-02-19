f0d09b37247bce7e0ed18d90a4c3946cec3bcbfb
Salvo-Raia <salvo.raia@outlook.it>
2026-02-19 17:22:06 +0100

    Fix fetch to get proper product detail page

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -1,20 +1,19 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
-const fakeStoreProducts = "https://fakestoreapi.com/products";
 
 export default function ProductDetailPage() {
   const { id } = useParams();
   const [productCard, setProductCard] = useState([]);
 
   const fetchProducts = () => {
-    axios.get(fakeStoreProducts).then((res) => {
-      console.log(res.data[id - 1]);
-      setProductCard(res.data[id - 1]);
+    axios.get(`https://fakestoreapi.com/products/${id}`).then((res) => {
+      console.log(res.data);
+      setProductCard(res.data);
     });
   };
 
   useEffect(fetchProducts, []);
 
-  return <h2>Ti mostro la scheda di {productCard?.title}</h2>;
+  return;
 }
