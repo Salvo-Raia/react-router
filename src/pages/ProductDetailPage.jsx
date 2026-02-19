f23875075c79d7b5e6777204e531a695f21ec13e
Salvo-Raia <salvo.raia@outlook.it>
2026-02-19 17:02:33 +0100

    Minor fix to Product Detail Page

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -1,18 +1,20 @@
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 const fakeStoreProducts = "https://fakestoreapi.com/products";
 
 export default function ProductDetailPage() {
   const { id } = useParams();
+  const [productCard, setProductCard] = useState([]);
 
   const fetchProducts = () => {
     axios.get(fakeStoreProducts).then((res) => {
       console.log(res.data[id - 1]);
+      setProductCard(res.data[id - 1]);
     });
   };
 
-  useEffect(() => fetchProducts);
+  useEffect(fetchProducts, []);
 
-  return <h2>Ti mostro la scheda del prodotto </h2>;
+  return <h2>Ti mostro la scheda di {productCard?.title}</h2>;
 }
